18e17ff31e6f56c8695e357c606136c9c548a713
Gary <odeeew@gmail.com>
2016-05-18 11:25:36 +0800

    加上 Work 的卡片展開特效

--- js/main.js
+++ js/main.js
@@ -1,131 +1,132 @@
-$(document).ready(function(){
+jQuery.noConflict();
+jQuery(document).ready(function(){
 
 });
 
-$('#menu span').click(function () {
-    var target = $(this).attr('id').split('menu',2)[1];
-    $('html body').animate({
-       scrollTop:(parseInt(parent.$('#' + target).offset().top) - 54) + 'px'
+jQuery('#menu span').click(function () {
+    var target = jQuery(this).attr('id').split('menu',2)[1];
+    jQuery('html body').animate({
+       scrollTop:(parseInt(parent.jQuery('#' + target).offset().top) - 54) + 'px'
     },1000);
 });
 
-$(window).scroll(function () {
+jQuery(window).scroll(function () {
 
-    var scrollHeight =  parseInt($('html body').scrollTop()),
-        viewportHeight = parseInt($(window).height()),
-        skillPosition = Math.round(parseInt($('#skillContainer').offset().top));
+    var scrollHeight =  parseInt(jQuery('html body').scrollTop()),
+        viewportHeight = parseInt(jQuery(window).height()),
+        skillPosition = Math.round(parseInt(jQuery('#skillContainer').offset().top));
 
      if (scrollHeight >= (viewportHeight - 54)){
-         if ($('#coverPlaceholder').length == 0){
-             $('#container').prepend('<div id="coverPlaceholder" style="height: 100vh"></div>');
+         if (jQuery('#coverPlaceholder').length == 0){
+             jQuery('#container').prepend('<div id="coverPlaceholder" style="height: 100vh"></div>');
          }
-         $('#cover').css({'position':'fixed','top':-(viewportHeight-54) + 'px','z-index':'999','opacity':'0.9'});
+         jQuery('#cover').css({'position':'fixed','top':-(viewportHeight-54) + 'px','z-index':'999','opacity':'0.9'});
      } else {
-         $('#coverPlaceholder').remove();
-         $('#cover').css({'position':'relative','top':'0','opacity':'1'});
+         jQuery('#coverPlaceholder').remove();
+         jQuery('#cover').css({'position':'relative','top':'0','opacity':'1'});
      }
 
     if ((scrollHeight + viewportHeight) - skillPosition > 0) {
-        $('.skill').fadeIn(500);
-        if ($('.skill').css('opacity') == 0){
-            $('#photoshop').ClassyLoader({
+        jQuery('.skill').fadeIn(500);
+        if (jQuery('.skill').css('opacity') == 0){
+            jQuery('#photoshop').ClassyLoader({
                 percentage:90,
                 roundedLine:true,
                 lineColor:'rgba(0, 160, 204, 1)',
                 fontColor:'rgba(0, 160, 204, 1)'
             }).draw()
-            $('#photoshop').parent('div').css('color','rgba(0, 160, 204, 1)');
+            jQuery('#photoshop').parent('div').css('color','rgba(0, 160, 204, 1)');
 
-            $('#illustrator').ClassyLoader({
+            jQuery('#illustrator').ClassyLoader({
                 percentage:75,
                 roundedLine:true,
                 lineColor:'rgba(204, 99, 0, 1)',
                 fontColor:'rgba(204, 99, 0, 1)'
             }).draw();
-            $('#illustrator').parent('div').css('color','rgba(204, 99, 0, 1)');
+            jQuery('#illustrator').parent('div').css('color','rgba(204, 99, 0, 1)');
 
-            $('#sketch').ClassyLoader({
+            jQuery('#sketch').ClassyLoader({
                 percentage:80,
                 roundedLine:true,
                 lineColor:'rgba(198, 159, 54, 1)',
                 fontColor:'rgba(198, 159, 54, 1)'
             }).draw();
-            $('#sketch').parent('div').css('color','rgba(198, 159, 54, 1)');
+            jQuery('#sketch').parent('div').css('color','rgba(198, 159, 54, 1)');
 
-            $('#pixate').ClassyLoader({
+            jQuery('#pixate').ClassyLoader({
                 percentage:75,
                 roundedLine:true,
                 lineColor:'rgba(0, 129, 178, 1)',
                 fontColor:'rgba(0, 129, 178, 1)'
             }).draw();
-            $('#pixate').parent('div').css('color','rgba(0, 129, 178, 1)');
+            jQuery('#pixate').parent('div').css('color','rgba(0, 129, 178, 1)');
 
-            $('#axure').ClassyLoader({
+            jQuery('#axure').ClassyLoader({
                 percentage:75,
                 roundedLine:true,
                 lineColor:'rgba(185, 32, 106, 1)',
                 fontColor:'rgba(185, 32, 106, 1)'
             }).draw();
-            $('#axure').parent('div').css('color','rgba(185, 32, 106, 1)');
+            jQuery('#axure').parent('div').css('color','rgba(185, 32, 106, 1)');
 
-            $('#hype').ClassyLoader({
+            jQuery('#hype').ClassyLoader({
                 percentage:80,
                 roundedLine:true,
                 lineColor:'rgba(188, 129, 60, 1)',
                 fontColor:'rgba(188, 129, 60, 1)'
             }).draw();
-            $('#hype').parent('div').css('color','rgba(188, 129, 60, 1)');
+            jQuery('#hype').parent('div').css('color','rgba(188, 129, 60, 1)');
 
-            $('#webstorm').ClassyLoader({
+            jQuery('#webstorm').ClassyLoader({
                 percentage:80,
                 roundedLine:true,
                 lineColor:'rgba(10, 174, 182, 1)',
                 fontColor:'rgba(10, 174, 182, 1)'
             }).draw();
-            $('#webstorm').parent('div').css('color','rgba(10, 174, 182, 1)');
+            jQuery('#webstorm').parent('div').css('color','rgba(10, 174, 182, 1)');
 
-            $('#html5').ClassyLoader({
+            jQuery('#html5').ClassyLoader({
                 percentage:75,
                 roundedLine:true,
                 lineColor:'rgba(193, 81, 33, 1)',
                 fontColor:'rgba(193, 81, 33, 1)'
             }).draw();
-            $('#html5').parent('div').css('color','rgba(193, 81, 33, 1)');
+            jQuery('#html5').parent('div').css('color','rgba(193, 81, 33, 1)');
 
-            $('#css3').ClassyLoader({
+            jQuery('#css3').ClassyLoader({
                 percentage:80,
                 roundedLine:true,
                 lineColor:'rgba(33, 135, 178, 1)',
                 fontColor:'rgba(33, 135, 178, 1)'
             }).draw();
-            $('#css3').parent('div').css('color','rgba(33, 135, 178, 1)');
+            jQuery('#css3').parent('div').css('color','rgba(33, 135, 178, 1)');
 
-            $('#jquery').ClassyLoader({
+            jQuery('#jquery').ClassyLoader({
                 percentage:80,
                 roundedLine:true,
                 lineColor:'rgba(96, 166, 196, 1)',
                 fontColor:'rgba(96, 166, 196, 1)'
             }).draw();
-            $('#jquery').parent('div').css('color','rgba(96, 166, 196, 1)');
+            jQuery('#jquery').parent('div').css('color','rgba(96, 166, 196, 1)');
 
-            $('#bootstrap').ClassyLoader({
+            jQuery('#bootstrap').ClassyLoader({
                 percentage:70,
                 roundedLine:true,
                 lineColor:'rgba(73, 52, 103, 1)',
                 fontColor:'rgba(73, 52, 103, 1)'
             }).draw();
-            $('#bootstrap').parent('div').css('color','rgba(73, 52, 103, 1)');
+            jQuery('#bootstrap').parent('div').css('color','rgba(73, 52, 103, 1)');
 
-            $('#rwd').ClassyLoader({
+            jQuery('#rwd').ClassyLoader({
                 percentage:85,
                 roundedLine:true,
                 lineColor:'rgba(86, 160, 155, 1)',
                 fontColor:'rgba(86, 160, 155, 1)'
             }).draw();
-            $('#rwd').parent('div').css('color','rgba(86, 160, 155, 1)');
+            jQuery('#rwd').parent('div').css('color','rgba(86, 160, 155, 1)');
 
-            if (parseInt($(window).width()) < 768){
-                $('.skill').css({'width':'50%'});
+            if (parseInt(jQuery(window).width()) < 768){
+                jQuery('.skill').css({'width':'50%'});
             }
         }
     }
