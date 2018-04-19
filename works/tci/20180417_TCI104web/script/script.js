9de03b93bc6b4a99865a227a68ca2af953614f7c
Gary <odeeew@gmail.com>
2018-04-19 15:34:23 +0800

    調整大江徵才頁在手機上影片無法自動播放時的處理

--- works/tci/20180417_TCI104web/script/script.js
+++ works/tci/20180417_TCI104web/script/script.js
@@ -3,7 +3,6 @@ jQuery.noConflict();
 jQuery(document).ready(function() {
 
 	let vw = jQuery(window).width();
-	let v1Height = vw / 1920 * 690;
 
 	if (vw < 414 || vw === 414 ){
 		jQuery('iframe').attr('height','500');
