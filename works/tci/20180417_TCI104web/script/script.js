a853b19b37a31fb0b48b5e3b1184e42140c2eb01
Gary <odeeew@gmail.com>
2018-04-19 15:10:46 +0800

    調整大江徵才頁在手機上的標題，以及影片無法自動播放時的處理

--- works/tci/20180417_TCI104web/script/script.js
+++ works/tci/20180417_TCI104web/script/script.js
@@ -1,11 +1,12 @@
 jQuery.noConflict();
+
 jQuery(document).ready(function() {
 
 	let vw = jQuery(window).width();
 	let v1Height = vw / 1920 * 690;
-	jQuery('.video1 iframe').attr('height',v1Height);
 
 	if (vw < 414 || vw === 414 ){
 		jQuery('iframe').attr('height','500');
+		jQuery('.video1 video').attr('controls','');
 	}
 });
